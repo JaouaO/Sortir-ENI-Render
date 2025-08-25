57e2c356fd379fef10b134fb9dd3b058dade963d
0oXiwiXo0 <christophe.rbrto@gmail.com>
2025-08-25 16:10:46 +0200

    Supression de SecurityController et MAJ de UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,12 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+#[Route('/utilisateur', name: 'user')]
 final class UserController extends AbstractController
 {
     #[Route('/index', name: 'app_index')]
     public function index(): Response
     {
         return $this->render('user/index.html.twig');
+    }
+
+    #[Route(path: '/connexion', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
