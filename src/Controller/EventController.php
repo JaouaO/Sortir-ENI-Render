db684d0496524b83ddfe8a324132fa24b9814bf2
Jaoua OLLIVIER <jaoua.ollivier2025@campus-eni.fr>
2025-08-25 15:40:20 +0200

    fix/basenavbar

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -9,11 +9,6 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/sortie', name: 'event')]
 final class EventController extends AbstractController
 {
-    #[Route('/{id}', name: '_display')]
-    public function display(int $id): Response
-    {
-        return $this->render('event/display.html.twig',['id' => $id]);
-    }
     #[Route('/creer', name: '_create')]
     public function create(): Response
     {
@@ -28,5 +23,10 @@ final class EventController extends AbstractController
     public function cancel(int $id): Response
     {
         return $this->render('event/cancel.html.twig',['id' => $id]);
+    }
+    #[Route('/{id}', name: '_display')]
+    public function display(int $id): Response
+    {
+        return $this->render('event/display.html.twig',['id' => $id]);
     }
 }
