2eafea7c0000b7bcda044c0209bab7bf572eec09
EricG44 <eric.guillouet@gmail.com>
2025-08-27 12:10:50 +0200

    Liste des sorties/ Filtre par site Ok

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,16 +7,25 @@ use App\Repository\EventRepository;
 use App\Repository\SiteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
     #[Route(['/','/accueil'], name: 'home')]
-    public function index(EventRepository $eventRepository, SiteRepository $siteRepository, EntityManagerInterface $em): Response
+    public function index(EventRepository $eventRepository, SiteRepository $siteRepository, EntityManagerInterface $em, Request $request): Response
     {
-        $events = $eventRepository->findAll();
+        //Recherche par site
         $sites = $siteRepository->findAll();
+        $siteId = $request->query->get('site');
+        if ($siteId) {
+            $events = $eventRepository->findBy(['site' => $siteId]);
+        } else {
+            $events = $eventRepository->findAll();
+        }
+
+
 
 
         return $this->render('main/index.html.twig', [
