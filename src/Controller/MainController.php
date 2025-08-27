af8b0401d974889bbd6bd1c511f68b40933e2cf8
EricG44 <eric.guillouet@gmail.com>
2025-08-27 09:26:34 +0200

    Init Search event

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Form\EventType;
 use App\Repository\EventRepository;
+use App\Repository\SiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,16 +13,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MainController extends AbstractController
 {
     #[Route(['/','/accueil'], name: 'home')]
-    public function index(EventRepository $eventRepository): Response
+    public function index(EventRepository $eventRepository, SiteRepository $siteRepository, EntityManagerInterface $em): Response
     {
         $events = $eventRepository->findAll();
-
-        // Création du formulaire (adapter EventType selon le formulaire réel)
-        $form = $this->createForm(EventType::class);
+        $sites = $siteRepository->findAll();
 
         return $this->render('event/display.html.twig', [
             'events' => $events,
-            'events_form' => $form->createView(),
+            'sites' => $sites
         ]);
     }
 }
