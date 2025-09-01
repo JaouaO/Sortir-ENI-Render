8a8a0fce1fc8100e54994ff0ba2e3a4aaf2000b1
0oXiwiXo0 <christophe.rbrto@gmail.com>
2025-09-01 13:59:37 +0200

    création methode et page import via CSV

---
+++ src/Form/UserImportType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+
+class UserImportType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('csvFile', FileType::class, [
+            'label' => 'Fichier CSV',
+            'mapped' => false, // Ici le champ n’est pas relié à l’entité User
+            'required' => true,
+            'constraints' => [
+                new File([
+                    'mimeTypes' => [
+                        "text/csv",
+                        'text/plain',
+                        'application/vnd.ms-excel',
+                    ],
+                    'mimeTypesMessage' => 'Merci de télécharger un fichier CSV valide',
+                ]),
+            ],
+        ]);
+    }
+
+}
