ff183d2ba03d27fc3850f60709ac8b089a5b85b0
Pierre-Jon <pierre.jonchere@gmail.com>
2025-09-01 13:46:27 +0200

    Ajout fonction modifier une ville

--- src/Controller/ManagerController.php
+++ src/Controller/ManagerController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/gestion', name: 'manager')]
 final class ManagerController extends AbstractController
@@ -28,7 +29,73 @@ final class ManagerController extends AbstractController
         ]);
     }
 
-    #[Route('//{id}/supprimer', name: '_delete')]
+//    #[Route('/{id}/creer', name: '_create', requirements: ['id' => '\d+'])]
+//    public function createCity(
+//        CityRepository $cityRepository,
+//        Request $request,
+//        City $city,
+//        EntityManagerInterface $em,
+//    ): Response {
+//
+//        if ($request->isMethod('POST')) {
+//            $town = $request->request->get('town');
+//            $cp = $request->request->get('cp');
+//
+//            $city->setName($town);
+//            $city->setPostCode($cp);
+//
+//            $em->persist($city);
+//            $em->flush();
+//
+//            $this->addFlash('success', "La ville {$city->getName()} a été modifié");
+//            return $this->redirectToRoute('manager_cities');
+//        }
+//
+//        $cities = $cityRepository->findAll();
+//
+//        return $this->render('manager/update-city.html.twig', [
+//            'mode' => 'create',
+//            'cities' => $cities,
+//            'city' => $city, // Pour pré-remplir le formulaire
+//        ]);
+//    }
+
+
+
+    #[Route('/{id}/modifier', name: '_update', requirements: ['id' => '\d+'])]
+    public function updateCity(
+        CityRepository $cityRepository,
+        Request $request,
+        City $city,
+        EntityManagerInterface $em,
+        ): Response {
+
+        if ($request->isMethod('POST')) {
+            $town = $request->request->get('town');
+            $cp = $request->request->get('cp');
+
+                $city->setName($town);
+                $city->setPostCode($cp);
+
+                $em->persist($city);
+                $em->flush();
+
+                $this->addFlash('success', "La ville {$city->getName()} a été modifié");
+                return $this->redirectToRoute('manager_cities');
+            }
+
+        $cities = $cityRepository->findAll();
+
+        return $this->render('manager/update-city.html.twig', [
+            'mode' => 'update',
+            'cities' => $cities,
+            'city' => $city, // Pour pré-remplir le formulaire
+        ]);
+    }
+
+
+
+    #[Route('//{id}/supprimer', name: '_delete', requirements: ['id' => '\d+'])]
     public function deleteCity(int $id, EntityManagerInterface $em): Response {
 
         $city = $em->getRepository(City::class)->find($id);
