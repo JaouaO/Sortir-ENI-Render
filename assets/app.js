18cb92948c46baf53b4e359cee71fa2599b4f5af
Jaoua OLLIVIER <jaoua.ollivier2025@campus-eni.fr>
2025-09-04 15:32:13 +0200

    m

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,8 +6,13 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 
-import './styles/app.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap';
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+import * as Turbo from "@hotwired/turbo"
 
+window.Turbo = Turbo
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
